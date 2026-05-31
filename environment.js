8ebfdb5e5c3864b2d8d89309bb9c9b8ed7c37dfb
freya-BagelEnglish <freyafeng.yy@gmail.com>
2026-05-31 20:42:37 +0800

    Create environment.js

---
+++ environment.js
@@ -0,0 +1,14 @@
+const words = [
+  {
+    word: "habitat",
+    meaning: "栖息地"
+  },
+  {
+    word: "biodiversity",
+    meaning: "生物多样性"
+  },
+  {
+    word: "sustainable development",
+    meaning: "可持续发展"
+  }
+];
